9dae5e95406780e12086f09964018f0c15c71e5f
CrazyEvclyd <crazyevklid@gmail.com>
2016-07-02 18:25:27 +0300

    complete shell automation

---
+++ neoautomation.sh
@@ -0,0 +1,17 @@
+#!/usr/bin/env sh
+
+tmp="$(date +%Y-%m-%d)"
+arg1=$1
+save_path=$arg1$tmp
+find $arg1 -name "*.neo" -exec rm -rf {} \;
+if [ -d $save_path ]; then
+    rm -rf $save_path
+fi
+mkdir $save_path
+java -jar target/neoautomation-1.0-SNAPSHOT-jar-with-dependencies.jar $arg1
+#cp $arg1"*.neo" $save_path"/"
+find $arg1 -name "*.neo" -exec cp {} $save_path"/" \;
+#for f in $arg1"*.csv"
+#do
+#   cp -v $f $save_path"/"
+#done
